81df90f2f870bfdcd4b27b378e865520253c3ada
Alecampo86 <alejandrocampo_86@hotmail.com>
2023-06-13 00:27:24 -0300

    finished

--- dao-backend/solidity/server.js
+++ dao-backend/solidity/server.js
@@ -13,48 +13,7 @@ app.use((req, res, next) => {
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Example route to join the DAO
-// Example route to join the DAO
-// Example route to join the DAO
-app.post('/join-dao', async (req, res) => {
-    try {
-        // Get the address and privateKey from the request body
-        const { address, privateKey } = req.body;
-
-        // Create a wallet instance using the privateKey
-        const wallet = new hre.ethers.Wallet(privateKey, provider);
-
-        // Connect the DAO and token contracts with the wallet's signer
-        const daoContractWithSigner = daoContract.connect(wallet);
-        const tokenContractWithSigner = tokenContract.connect(wallet);
-
-        // Get the current token balance of the new member
-        const tokenBalanceBefore = await tokenContractWithSigner.balanceOf(address);
-
-        // Call the joinDAO function in the DAO contract with the client's address
-        await daoContractWithSigner.joinDAO({
-            value: 0,
-            gasLimit: 800000,
-            gasPrice: hre.ethers.utils.parseUnits("10", "gwei"),
-            nonce: await provider.getTransactionCount(address),
-        });
-
-        // Get the updated token balance of the new member
-        const tokenBalanceAfter = await tokenContractWithSigner.balanceOf(address);
-
-        // Calculate the tokens received by the new member
-        const tokensReceived = tokenBalanceAfter.sub(tokenBalanceBefore);
-
-        res.json({ message: 'Joined the DAO successfully', tokensReceived: tokensReceived.toString() });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to join the DAO' });
-    }
-});
-
-
-// Example route to fetch all proposals
+// fetch all proposals
 app.get('/proposals', async (req, res) => {
     try {
         // Connect to the deployed DAO contract
@@ -126,52 +85,6 @@ app.get('/proposals', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch proposals' });
-    }
-});
-
-// Example route to create a new proposal
-app.post('/proposals', async (req, res) => {
-    try {
-        const { title, description, deadline, minimumVotes } = req.body;
-
-        // Connect to the deployed DAO contract
-        const DAO = await hre.ethers.getContractFactory("DAO");
-        const daoContract = await DAO.attach('0x7814ea4D6AD4Ea503e17D27E6895236e4b224090');
-
-        // Create the proposal
-        await daoContract.createProposal(
-            title,
-            description,
-            deadline,
-            minimumVotes
-        );
-
-        res.json({ message: 'New proposal created' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to create a new proposal' });
-    }
-});
-
-// Example route to submit a vote for a proposal
-app.post('/proposals/:id/vote/:vote', async (req, res) => {
-    try {
-        const proposalId = req.params.id;
-        console.log('proposalId ', proposalId)
-        const voteOption = req.params.vote;
-        console.log('voteOption ', voteOption)
-
-        // Connect to the deployed DAO contract
-        const DAO = await hre.ethers.getContractFactory("DAO");
-        const daoContract = await DAO.attach('0x7814ea4D6AD4Ea503e17D27E6895236e4b224090');
-
-        // Submit the vote for the specified proposal
-        await daoContract.vote(proposalId, voteOption);
-
-        res.json({ message: `Vote submitted for proposal ${proposalId}, option ${voteOption}` });
-    } catch (error) {
-        console.error('the error is  ', error);
-        res.status(500).json({ error: 'Failed to submit the vote' });
     }
 });
 
